5c58b1c08113e0238175fa24f69c6cb4de458f3f
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2018-12-17 18:20:10 -0200

    Stateful vs Stateless

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from 'prop-types';
 import { render } from "react-dom";
 
-class Button extends Component {
-  static defaultProps = {
-    children: "Salvar"
-  };
-  
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-    children: PropTypes.string
-  };
-
-  render() {
-    return (
-      <button onClick={this.props.onClick}>
-       {this.props.children}
-      </button>
-    );
-  };
-}
+import Button from "./Button";
 
 class App extends Component {
   state = {
@@ -27,12 +9,7 @@ class App extends Component {
   };
 
   handleClick = () => {
-    this.setState({ counter: this.state.counter + 1 }, () => {
-      console.log(this.state.counter);
-    });
-
-    this.setState(state => ({ counter: state.counter + 1 }));
-    this.setState(state => ({ counter: state.counter + 1 }));
+    this.setState({ counter: this.state.counter + 1 });
   };
 
   render() {
